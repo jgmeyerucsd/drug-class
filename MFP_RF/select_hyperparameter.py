4e8ee9fe84d14c444d9e20b470241aef43b17289
chao1224 <shengchao.hacker@gmail.com>
2019-04-11 11:11:06 -0500

    Update CV hyper-parameter sweeping, got one job undone

--- MFP_RF/select_hyperparameter.py
+++ MFP_RF/select_hyperparameter.py
@@ -7,37 +7,55 @@ def extract(file_path):
     with open(file_path) as f:
         lines = f.readlines()
 
-    train_acc, test_acc = float('inf'), float('inf')
+    train_acc, val_acc, test_acc = float('inf'), float('inf'), float('inf')
     for line in lines:
         if 'train accuracy' in line:
             line = line.strip().split(':')
             train_acc = float(line[1])
+        if 'val accuracy' in line:
+            line = line.strip().split(':')
+            val_acc = float(line[1])
         if 'test accuracy' in line:
             line = line.strip().split(':')
             test_acc = float(line[1])
-    return train_acc, test_acc
+    return train_acc, val_acc, test_acc
 
 
 if __name__ == '__main__':
     class_num_list = [3, 5, 12]
+    K = 20
 
     for class_num in class_num_list:
         record = {}
+        for test_id in range(5):
+            record[test_id] = {}
+            for hyper_id in range(216):
+                record[test_id][hyper_id] = []
+
         for hyper_id in range(216):
-            train_acc_list, test_acc_list = [], []
-            for index in range(5):
-                file_path = 'output_fingerprints/num_class_{}_id_{}_index_{}.out'.format(class_num, hyper_id, index)
-                train_acc, test_acc = extract(file_path)
-                train_acc_list.append(train_acc)
-                test_acc_list.append(test_acc)
+            # TODO: debugging
+            if hyper_id in [187]:
+                continue
+            for idx in range(20):
+                test_id = idx / 4
+                val_id = idx % 4 + (idx % 4 >= test_id)
+                file_path = 'output_fingerprints/num_class_{}_id_{}_index_{}.out'.format(class_num, hyper_id, idx)
+                train_acc, val_acc, test_acc = extract(file_path)
+                record[test_id][hyper_id].append(val_acc)
 
-            mean_train_acc, mean_test_acc = np.mean(train_acc_list), np.mean(test_acc_list)
-            record[hyper_id] = mean_test_acc
+        for test_id in range(5):
+            for hyper_id in range(216):
+                if len(record[test_id][hyper_id]) == 0:
+                    record[test_id][hyper_id] = 0
+                else:
+                    record[test_id][hyper_id] = np.mean(record[test_id][hyper_id])
+            
+            ordered_record =  sorted(record[test_id].iteritems(), key=lambda (k,v):(v,k), reverse=True)
 
-        print('Class Num: {}'.format(class_num))
-        record = sorted(record.iteritems(), key=lambda (k,v):(v,k), reverse=True)
-        for k,v in record:
-            print('{}: {}'.format(k, v))
-        print()
+            print('Class Num: {}\tTop {} ({} fold is the test).'.format(class_num, K, test_id))
+            for k,v in ordered_record[:K]:
+                print('{}: {}'.format(k, v))
+            print()
+            print()
         print()
         print()
