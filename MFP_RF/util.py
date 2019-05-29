0a646eb61960386f42fafeccbd613ba74dc82632
chao1224 <shengchao.hacker@gmail.com>
2019-05-29 09:00:21 -0500

    Update ROC-AUC and PR-AUC in multi-class case

--- MFP_RF/util.py
+++ MFP_RF/util.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
 
 import numpy as np
-from sklearn.metrics import accuracy_score, roc_auc_score, matthews_corrcoef, balanced_accuracy_score
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, matthews_corrcoef, roc_auc_score, average_precision_score
+from sklearn.preprocessing import OneHotEncoder, label_binarize
 
 
 def reshape_data_into_2_dim(data):
@@ -11,26 +11,19 @@ def reshape_data_into_2_dim(data):
         data = data.reshape(n, 1)
     return data
 
-'''
-    ## get the roc weighted scores
-    log_preds, y = learn.TTA()
-    probs = np.mean(np.exp(log_preds),0)
-    roc.append(multiclass_roc_auc_score(y, probs, 'weighted'))
-
-    # balanced accuracy
-    preds = np.argmax(probs, axis=1)
-    bacc.append(balanced_accuracy_score(y, preds))
-
-    # MCC
-    mcc.append(matthews_corrcoef(y, preds))
-'''
-
 
 def multiclass_roc_auc_score(y_true, y_prob, AVERAGE='weighted'):
-    y_prob = np.mean(y_prob)
-    oh = OneHotEncoder(sparse=False, categories='auto')
-    yt = oh.fit_transform(y_true.reshape(-1,1))
-    return roc_auc_score(yt, y_prob, average=AVERAGE)
+    n_classes = int(max(max(y_true), max(y_prob)) + 1)
+    y_true = label_binarize(y_true, classes=[x for x in range(n_classes)])
+    y_prob = label_binarize(y_prob, classes=[x for x in range(n_classes)])
+    return roc_auc_score(y_true, y_prob, average=AVERAGE)
+
+
+def multiclass_pr_auc_score(y_true, y_prob, AVERAGE='weighted'):
+    n_classes = int(max(max(y_true), max(y_prob)) + 1)
+    y_true = label_binarize(y_true, classes=[x for x in range(n_classes)])
+    y_prob = label_binarize(y_prob, classes=[x for x in range(n_classes)])
+    return average_precision_score(y_true, y_prob, average=AVERAGE)
 
 
 def output_classification_result(y_train, y_pred_on_train,
@@ -38,20 +31,23 @@ def output_classification_result(y_train, y_pred_on_train,
                                  y_test, y_pred_on_test):
     if y_train is not None:
         print('train accuracy: {}'.format(accuracy_score(y_true=y_train, y_pred=y_pred_on_train)))
-        # print('train multiclass AUC[ROC]: {}'.format(multiclass_roc_auc_score(y_true=y_train, y_prob=y_pred_on_train, AVERAGE='weighted')))
         print('train balanced accuracy: {}'.format(balanced_accuracy_score(y_true=y_train, y_pred=y_pred_on_train)))
         print('train matthews corrcoef: {}'.format(matthews_corrcoef(y_true=y_train, y_pred=y_pred_on_train)))
+        print('train multiclass ROC-AUC: {}'.format(multiclass_roc_auc_score(y_true=y_train, y_prob=y_pred_on_train, AVERAGE='weighted')))
+        print('train multiclass PR-AUC: {}'.format(multiclass_pr_auc_score(y_true=y_train, y_prob=y_pred_on_train, AVERAGE='weighted')))
 
     if y_val is not None:
         print('val accuracy: {}'.format(accuracy_score(y_true=y_val, y_pred=y_pred_on_val)))
-        # print('val multiclass AUC[ROC]: {}'.format(multiclass_roc_auc_score(y_true=y_val, y_prob=y_pred_on_val, AVERAGE='weighted')))
         print('val balanced accuracy: {}'.format(balanced_accuracy_score(y_true=y_val, y_pred=y_pred_on_val)))
         print('val matthews corrcoef: {}'.format(matthews_corrcoef(y_true=y_val, y_pred=y_pred_on_val)))
+        print('train multiclass ROC-AUC: {}'.format(multiclass_roc_auc_score(y_true=y_val, y_prob=y_pred_on_val, AVERAGE='weighted')))
+        print('val multiclass PR-AUC: {}'.format(multiclass_pr_auc_score(y_true=y_val, y_prob=y_pred_on_val, AVERAGE='weighted')))
 
     if y_test is not None:
         print('test accuracy: {}'.format(accuracy_score(y_true=y_test, y_pred=y_pred_on_test)))
-        # print('test multiclass AUC[ROC]: {}'.format(multiclass_roc_auc_score(y_true=y_test, y_prob=y_pred_on_test, AVERAGE='weighted')))
         print('test balanced accuracy: {}'.format(balanced_accuracy_score(y_true=y_test, y_pred=y_pred_on_test)))
         print('test matthews corrcoef: {}'.format(matthews_corrcoef(y_true=y_test, y_pred=y_pred_on_test)))
+        print('train multiclass ROC-AUC: {}'.format(multiclass_roc_auc_score(y_true=y_test, y_prob=y_pred_on_test, AVERAGE='weighted')))
+        print('test multiclass PR-AUC: {}'.format(multiclass_pr_auc_score(y_true=y_test, y_prob=y_pred_on_test, AVERAGE='weighted')))
 
     return
