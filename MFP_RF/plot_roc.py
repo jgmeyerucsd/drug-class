f2073d9d5a0366c4c7a979e38fc52f3a0bd14762
chao1224 <shengchao.hacker@gmail.com>
2019-05-31 09:44:10 -0500

    Update roc curve

--- MFP_RF/plot_roc.py
+++ MFP_RF/plot_roc.py
@@ -12,6 +12,13 @@ if __name__ == '__main__':
         12: [124, 65, 148, 88, 148]
     }
 
+    index_to_label = {
+        3: ['antineoplastic', 'cardio', 'cns'],
+        5: ['antiinfective', 'antineoplastic', 'cardio', 'cns', 'gastrointestinal'],
+        12: ['antiinfective', 'antiinflammatory', 'antineoplastic', 'cardio', 'cns', 'dermatologic',
+             'gastrointestinal', 'hematologic', 'lipidregulating', 'reproductivecontrol', 'respiratorysystem', 'urological']
+    }
+
     for class_num in [3, 5, 12]:
         data_file = 'output_all_single_class/num_class_{}_index_4.npz'.format(class_num)
         data = np.load(data_file)
@@ -21,6 +28,6 @@ if __name__ == '__main__':
 
         for x in range(class_num):
             fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba_on_test[:,x], pos_label=x)
-            plt.plot(fpr, tpr, label = x)
+            plt.plot(fpr, tpr, label=index_to_label[class_num][x])
         plt.legend()
         plt.savefig('plottings/{}_roc_curve.svg'.format(class_num), type='svg')
